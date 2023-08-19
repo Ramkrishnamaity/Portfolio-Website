1f4d251a6c0bb9f7f5fdf6b1b6a0bd6e0ddd3dac
Ramkrishna Maity <ramkrishnamaity4146@gmail.com>
2023-08-19 12:56:54 +0530

    final commit

---
+++ src/Data.js
@@ -0,0 +1,33 @@
+import todo from './assets/todo.jpg';
+import placement from './assets/placement.jpg'
+import profilecreator from './assets/profilecreator.jpg'
+
+
+const Data = [
+
+    {
+        image: placement,
+        title: 'Placementdecision.online',
+        desc: 'This project is about Placement Management web application where students can log in and apply for multiple job opportunities that college CDC will provide. Techs: ReactJS, NodeJS, ExpressJS',
+        link: 'https://github.com/Ramkrishnamaity/Placement-Website'
+        
+    },
+    {
+        image: profilecreator,
+        title: 'Profile Smart',
+        desc: "This is a profile creator application. Here users can log in and create their profile. Techs: ReactJS, Firebase",
+        link: 'https://ramkrishna-profile-creator-project.netlify.app/'
+
+    },
+    {
+        image: todo,
+        title: 'Todo App',
+        desc: 'This is a Todo Web app. Here users can add, delete as well as mark tasks. Techs: Front end languages such as HTML, CSS, JavaScript',
+        link: 'https://ramkrishna-todo-project.netlify.app/'
+
+    }
+
+]
+
+
+export default Data;
